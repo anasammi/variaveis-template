302825c00b36e69b2f99ef918e009204a90c8f98
anasammi <a.sammi1103@gmail.com>
2023-05-02 21:15:11 -0300

    exercícios da aula de variáveis

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const nome = prompt("Digite seu nome")
+
+let idade = Number(prompt("Digite sua idade"))
+
+// idade = Number(idade)
+
+let presenca = confirm("Confirme sua presença")
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof presenca)
+
+console.log("Estudante", nome, "tem", idade, "anos. Presença:", presenca)
+
+console.log("nome:", typeof nome, ", idade:", typeof idade, ", presença:", typeof presenca)
 
